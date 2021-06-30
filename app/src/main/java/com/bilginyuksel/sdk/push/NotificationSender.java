7e42422e474ea305f643367ab11e29f54488448f
mesutgedik <mesutgedikk@gmail.com>
2021-06-30 16:57:39 +0300

    MessageService added and NotificationSender edited.

--- app/src/main/java/com/bilginyuksel/sdk/push/NotificationSender.java
+++ app/src/main/java/com/bilginyuksel/sdk/push/NotificationSender.java
@@ -3,86 +3,41 @@ package com.bilginyuksel.sdk.push;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
-import android.os.Build;
 
-import androidx.core.app.NotificationCompat;
+import androidx.annotation.Nullable;
+
+import com.bilginyuksel.sdk.push.utils.Util;
 
 import java.util.concurrent.atomic.AtomicInteger;
-
 
 public class NotificationSender {
     private final static String CHANNEL_ID = "push-notification-channel";
     private final static String CHANNEL_NAME = "push-notification-channel";
 
-    private final static AtomicInteger ATOMIC_INTEGER = new AtomicInteger();
-
     private final Context context;
-
+    private final static AtomicInteger ATOMIC_INTEGER = new AtomicInteger();
     private NotificationChannel notificationChannel;
 
-    private int smallIcon;
-    private int priority;
-    private boolean autoCancel;
-
-
     public NotificationSender(Context context) {
-        this.context = context;
-
-        this.priority = NotificationCompat.PRIORITY_DEFAULT;
-        this.smallIcon = R.drawable.ic_launcher_background;
-        this.autoCancel = true;
-
-        if (isVersionBiggerThanOreo()) {
-            notificationChannel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
-        }
+            this.context = context;
+            if (Util.isVersionBiggerThanOreo()) {
+                notificationChannel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
+            }
     }
 
-    public void send(RemoteMessage remoteMessage) {
+    public void send(RemoteMessage remoteMessage, @Nullable NotificationChannel channel) {
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        Notification notification = new NotificationCompat.Builder(context, CHANNEL_ID)
-                .setContentTitle(remoteMessage.getTitle())
-                .setContentText(remoteMessage.getContent())
-                .setSmallIcon(smallIcon)
-                .setPriority(priority)
-                .setAutoCancel(autoCancel)
-                .setContentIntent(createPendingIntent())
-                .build();
-
         // If android version is bigger than oreo, notification channel is mandatory
-        if (isVersionBiggerThanOreo()) {
-            notificationManager.createNotificationChannel(notificationChannel);
+        if (Util.isVersionBiggerThanOreo()) {
+            if(channel != null)
+                notificationManager.createNotificationChannel(channel);
+            else
+                notificationManager.createNotificationChannel(notificationChannel);
         }
 
+        Notification notification = new NotificationBuilder(this.context).build(remoteMessage, CHANNEL_ID);
         notificationManager.notify(ATOMIC_INTEGER.incrementAndGet(), notification);
-    }
-
-    public void setNotificationChannel(NotificationChannel channel) {
-        this.notificationChannel = channel;
-    }
-
-    public void setAutoCancel(boolean autoCancel) {
-        this.autoCancel = autoCancel;
-    }
-
-    public void setPriority(int priority) {
-        this.priority = priority;
-    }
-
-    public void setSmallIcon(int smallIcon) {
-        this.smallIcon = smallIcon;
-    }
-
-    private PendingIntent createPendingIntent() {
-        Intent intent = new Intent(context, MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        return PendingIntent.getActivity(context, 0, intent, 0);
-    }
-
-    private boolean isVersionBiggerThanOreo() {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.O;
     }
 
 }
